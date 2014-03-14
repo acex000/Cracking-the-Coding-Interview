43752ff0704b27507444975444c96d6f5c8cb3a5
Chuan He <hechuan2g@gmail.com>
2014-03-14 15:58:18 -0400

    finished 1.4

--- Ch1_Arrays_and_Strings/1.4/replaceSpace.java
+++ Ch1_Arrays_and_Strings/1.4/replaceSpace.java
@@ -1,16 +1,40 @@
 
-public class replaceSpace {
+public class ReplaceSpace {
 
 
 	public static void main(String[] args){
 		
-		String str = "Mr John Smith    "
+		String str = "Mr John Smith     ";
+		int length = 13;
 		char[] charArr = str.toCharArray();
-
-		replace(charArr);
+		
+		replace(charArr,length);
+		System.out.println(new String(charArr));
 	}
 
-	public static void replace(char[] charArr){
+	public static void replace(char[] charArr, int length){
+
+		int spaceCount=0;
+		for(int i=0;i<length;i++){
+			if(charArr[i]==' ')
+				++spaceCount;
+		}
 		
+		int newLength = length+spaceCount*2;
+		
+		charArr[newLength]='\0';
+
+		for(int i=length-1, j=newLength-1;i>=0;--i,--j){
+			if(charArr[i]==' '){
+				charArr[j]='0';
+				--j;
+				charArr[j]='2';
+				--j;
+				charArr[j]='%';
+			}
+			else {
+				charArr[j]=charArr[i];
+			}
+		}		
 	}
 }
